1dd15dec8aa28147c648d44e659b964133518333
Rifat Mridha <64531857+rifatmridha2913@users.noreply.github.com>
2022-06-01 13:34:36 +1000

    Repo details - initial commit

---
+++ RepoDetails.sql
@@ -0,0 +1,6 @@
+/********************************************************************************
+Title:			Testing GitHub
+Pupose: 		This repo is for all personal use and coding
+Link to Git: 	https://github.com/rifatmridha2913/sandbox_playground
+Gitname: 		rifat.mridha2913@gmail.com
+********************************************************************************/
